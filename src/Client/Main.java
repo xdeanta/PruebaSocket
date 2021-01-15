8ab25dbb409c6b79cb7412705d7fdd40d7d16a77
Xavier <xdeantabelisario@danielcastelao.org>
2021-01-15 14:44:29 +0100

    Initial commit

---
+++ src/Client/Main.java
@@ -0,0 +1,40 @@
+package Client;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("Creando socket cliente");
+            Socket clienteSocket = new Socket();
+            System.out.println("Estableciendo la conexi�n");
+
+            InetSocketAddress addr = new InetSocketAddress("192.168.56.2", 5555);
+            clienteSocket.connect(addr);
+
+            InputStream is = clienteSocket.getInputStream();
+            OutputStream os = clienteSocket.getOutputStream();
+
+            System.out.println("Enviando mensaje");
+
+            String mensaje = "mensaje desde el cliente";
+            os.write(mensaje.getBytes());
+
+            System.out.println("Mensaje enviado");
+
+            System.out.println("Cerrando el socket cliente");
+
+            clienteSocket.close();
+
+            System.out.println("Terminado");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/Server/Main.java
@@ -0,0 +1,50 @@
+package Server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.ServerSocket;
+
+public class Main {
+
+    public static void main(String[] args){
+        try{
+            System.out.println("Creando socket servidor");
+
+            ServerSocket serverSocket=new ServerSocket();
+
+            System.out.println("Realizando el bind");
+
+            InetSocketAddress addr=new InetSocketAddress("192.168.56.2",5555);
+            serverSocket.bind(addr);
+
+            System.out.println("Aceptando conexiones");
+
+            Socket newSocket= serverSocket.accept();
+
+            System.out.println("Conexi�n recibida");
+
+            InputStream is=newSocket.getInputStream();
+            OutputStream os=newSocket.getOutputStream();
+
+            byte[] mensaje=new byte[25];
+            is.read(mensaje);
+
+            System.out.println("Mensaje recibido: "+new String(mensaje));
+
+            System.out.println("Cerrando el nuevo socket");
+
+            newSocket.close();
+
+            System.out.println("Cerrando el socket servidor");
+
+            serverSocket.close();
+
+            System.out.println("Terminado");
+
+        }catch (IOException e) {
+        }
+    }
+}
